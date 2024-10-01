51074949e9ea4b7123f5febbad855e28f8f7f57e
kumar-karan <kk9910083222@gmail.com>
2024-10-01 23:13:41 +0530

    Added matrix multiplication in C#

--- Matrix-manipulation.cpp
+++ Matrix-manipulation.cpp
@@ -1,62 +1,97 @@
-#include<iostream>
-#include<iomanip>
-#include<stdlib.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
-int  main()
-{
-    int t;
-    int x,y;
-    int r,c,q,cnt=0;
-    int *p;
-    cin>>t;
-    while(t--)
-    {
-        cin>>r>>c>>q;
-        int a[r][c]={0};
-        p=&a[0][0];
 
-        for(int i=0;i<c;i++)
-            a[0][i]=0;
-
-        while(q--)
-        {
-            cin>>x>>y;
-            x--;
-            y--;
+void inputMatrix(vector<vector<int> >& matrix, int rows, int cols) {
+    cout << "Enter the elements of the matrix: \n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
         }
-
-		for(int i=0;i<r*c;i++)
-	    {
-	    	int item;
-	    	item=*p;
-	    	if(item%2!=0)
-	    	cnt++;
-	    	p++;
-	    }
-	cout<<endl;	
-	}
-        for(int i=0;i<r*c;i++)
-        {
-            int item;
-            item=*p;
-            if(item%2!=0)
-            cnt++;
-            p++;
-        }
-
-        cout<<cnt<<endl;    
     }
-
-    for(int i=0;i<r*c;i++)
-    {
-        int item;
-        item=*p;
-        if(item%2!=0)
-        cnt++;
-        p++;
-    }
-
-    cout<<cnt<<endl;    
 }
 
+void displayMatrix(const vector<vector<int> >& matrix, int rows, int cols) {
+    cout << "Matrix: \n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void addMatrices(const vector<vector<int> >& a, const vector<vector<int> >& b, vector<vector<int> >& c, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+void subtractMatrices(const vector<vector<int> >& a, const vector<vector<int> >& b, vector<vector<int> >& c, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+}
+
+void multiplyMatrices(const vector<vector<int> >& a, const vector<vector<int> >& b, vector<vector<int> >& c, int m, int n, int p) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int m, n, o, p, choice;
+    cout << "Choose operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n";
+    cin >> choice;
+
+    cout << "Enter rows and columns for matrix A (m * n): ";
+    cin >> m >> n;
+    vector<vector<int> > a(m, vector<int>(n));
+    inputMatrix(a, m, n);
+
+    cout << "Enter rows and columns for matrix B (o * p): ";
+    cin >> o >> p;
+    vector<vector<int> > b(o, vector<int>(p));
+
+    if (choice != 3) {
+        if (m != o || n != p) {
+            cout << "Matrix operation not possible due to incompatible sizes." << endl;
+            return 0;
+        }
+    }
+    
+    inputMatrix(b, o, p);
+    vector<vector<int> > c(m, vector<int>(n));
+
+    switch (choice) {
+    case 1:
+        addMatrices(a, b, c, m, n);
+        displayMatrix(c, m, n);
+        break;
+    case 2:
+        subtractMatrices(a, b, c, m, n);
+        displayMatrix(c, m, n);
+        break;
+    case 3:
+        if (n != o) {
+            cout << "Multiplication not possible due to incompatible sizes." << endl;
+            return 0;
+        }
+        c.resize(m, vector<int>(p));
+        multiplyMatrices(a, b, c, m, n, p);
+        displayMatrix(c, m, p);
+        break;
+    default:
+        cout << "Invalid choice." << endl;
+    }
+    return 0;
+}
--- MatrixMultiplication.cs
+++ MatrixMultiplication.cs
@@ -1,28 +1,29 @@
 ﻿using System;
+
 class MatrixMultiplication
 {
     int[,] a;
     int[,] b;
     int[,] c;
     public int m1, n1, m2, n2;
+
     public void ReadMatrix()
     {
-        Console.WriteLine("\n Size of Matrix 1:");
-        Console.Write("\n Enter the number of rows in Matrix 1 :");
-        m1 = Convert.ToInt16(Console.ReadLine());
-        Console.Write("\n Enter the number of columns in Matrix 1 :");
-        n1 = Convert.ToInt16(Console.ReadLine());
+        Console.Write("\n Enter the number of rows for Matrix 1: ");
+        m1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write(" Enter the number of columns for Matrix 1: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
         a = new int[m1, n1];
-        Console.WriteLine("\n Size of Matrix 2 :");
-        Console.Write("\n Enter the number of rows in Matrix 2 :");
-        m2 = Convert.ToInt16(Console.ReadLine());
-        Console.Write("\n Enter the number of columns in Matrix 2 :");
-        n2 = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("\n Enter the number of rows for Matrix 2: ");
+        m2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write(" Enter the number of columns for Matrix 2: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
         b = new int[m2, n2];
+
         if (n1 != m2)
         {
-            Console.WriteLine("columns of A & Rows of B matrix are not equal");
-            Console.ReadKey();
+            Console.WriteLine("Multiplication not possible. The number of columns in Matrix 1 must equal the number of rows in Matrix 2.");
             Environment.Exit(0);
         }
         else
@@ -32,50 +33,21 @@ class MatrixMultiplication
             {
                 for (int j = 0; j < n1; j++)
                 {
-                    a[i, j] = Convert.ToInt16(Console.ReadLine());
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
+
             Console.WriteLine("\n Enter the elements of Matrix 2:");
             for (int i = 0; i < m2; i++)
             {
                 for (int j = 0; j < n2; j++)
                 {
-                    b[i, j] = Convert.ToInt16(Console.ReadLine());
+                    b[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
         }
     }
-    public void PrintMatrix()
-    {
-        Console.WriteLine("\n Matrix 1:");
-        for (int i = 0; i < m1; i++)
-        {
-            for (int j = 0; j < n1; j++)
-            {
-                Console.Write("\t" + a[i, j]);
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine("\n Matrix 2:");
-        for (int i = 0; i < m2; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-                Console.Write("\t" + b[i, j]);
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine("\n Resultant Matrix after multiplying:");
-        for (int i = 0; i < m1; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-                Console.Write("\t" + c[i, j]);
-            }
-            Console.WriteLine();
-        }
-        Console.ReadLine();
-    }
+
     public void MultiplyMatrix()
     {
         c = new int[m1, n2];
@@ -84,11 +56,28 @@ class MatrixMultiplication
             for (int j = 0; j < n2; j++)
             {
                 c[i, j] = 0;
-                for (int k = 0; k < n1; k++) c[i, j] = c[i, j] + a[i, k] * b[k, j];
+                for (int k = 0; k < n1; k++)
+                {
+                    c[i, j] += a[i, k] * b[k, j];
+                }
             }
         }
     }
+
+    public void PrintMatrix()
+    {
+        Console.WriteLine("\n Resultant Matrix:");
+        for (int i = 0; i < m1; i++)
+        {
+            for (int j = 0; j < n2; j++)
+            {
+                Console.Write("\t" + c[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
 }
+
 class Matrices
 {
     public static void Main()
